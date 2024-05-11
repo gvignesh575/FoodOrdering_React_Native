615726551322a9e85244fae94f057e5b3c036fa2
GVignesh-SD <gvignesh380980@gmail.com>
2024-05-11 14:55:19 +0530

    protected the admin route

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -6,7 +6,7 @@ import { useAuth } from '../providers/AuthProvider';
 import { supabase } from '../lib/supabase';
 
 const index = () => {
-  const { session,loading } = useAuth();
+  const { session,loading,isAdmin } = useAuth();
 
   if (loading)
     {
@@ -17,6 +17,11 @@ const index = () => {
       return <Redirect href={'/sign-in'} />
     }
 
+  if(!isAdmin)
+    {
+      return <Redirect href={'/(user)'} />
+    }
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', padding: 10 }}>
       <Link href={'/(user)'} asChild>
@@ -25,11 +30,7 @@ const index = () => {
       <Link href={'/(admin)'} asChild>
         <Button text="Admin" />
       </Link>
-      <Link href={'/sign-in'} asChild>
-        <Button text="Sign in" />
-      </Link>
       <Button onPress={() => supabase.auth.signOut()} text='Sign out'>
-
       </Button>
     </View>
   );
