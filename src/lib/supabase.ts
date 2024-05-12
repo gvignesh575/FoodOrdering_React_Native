2621aa2eb71b43ddb808486f74ff717a2a84462c
GVignesh-SD <gvignesh380980@gmail.com>
2024-05-12 12:49:34 +0530

    integrated the supabse

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,7 @@
 import 'react-native-url-polyfill/auto';
 import * as SecureStore from 'expo-secure-store';
 import { createClient } from '@supabase/supabase-js';
+import { Database } from '../database.types';
 
 const ExpoSecureStoreAdapter = {
   getItem: (key: string) => {
@@ -17,7 +18,7 @@ const ExpoSecureStoreAdapter = {
 const supabaseUrl = 'https://czvitwkqjmzlvkmiglvy.supabase.co';
 const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6dml0d2txam16bHZrbWlnbHZ5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTUzNDgwNTYsImV4cCI6MjAzMDkyNDA1Nn0.sLJsEZPppiWohRri5F_F4X7UiuPzqjmxcuIpBFy4y54';
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
+export const supabase = createClient<Database>(supabaseUrl, supabaseAnonKey, {
   auth: {
     storage: ExpoSecureStoreAdapter as any,
     autoRefreshToken: true,
--- src/providers/CartProvider.tsx
+++ src/providers/CartProvider.tsx
@@ -1,7 +1,8 @@
 import { PropsWithChildren, createContext, useContext, useState } from "react";
-import { CartItem, Product } from "../types";
+import { CartItem, Tables } from "../types";
 import { randomUUID } from "expo-crypto";
 
+type Product = Tables<'products'>
 
 type CartType = {
     items: CartItem[],
